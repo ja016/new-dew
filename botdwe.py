fe398cfeb3ba7a99998f6dfb5add2e630b5d5377
ja016 <61541468+ja016@users.noreply.github.com>
2020-02-27 22:23:54 +0900

    Add files via upload

---
+++ botdwe.py
@@ -0,0 +1,35 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("안녕"):
+        await message.channel.send("응")
+    if message.content.startswith("잘가"):
+        await message.channel.send("또만나")
+    if message.content.startswith("나야"):
+        await message.channel.send("오우 심했다")
+    if message.content.startswith("넌 나야"):
+        await message.channel.send("넌 어반")
+    if message.content.startswith("잘지내자"):
+        await message.channel.send("응")
+
+    if message.content.startswith("넌 누가만들어줬어?"):
+        await message.channel.send("스타렉스 어반이(가)만들어줬어")
+    if message.content.startswith("넌 누가 패치해줄거야?"):
+        await message.channel.send("앞으로는 스타렉스 어반이(가)패치할거야")
+    if message.content.startswith("알았어"):
+        await message.channel.send("응")
+
+client.run("NjY5MzUyMzczMjQzNjA5MTQx.XlfClg.2vAlETBXUpWPq2jGoDh__-57G1c")
+
+
+
